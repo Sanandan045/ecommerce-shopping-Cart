a021c39f58db00a97bc7dd8010485cde622a4ca0
Sanandan <sanandan200@gmail.com>
2023-07-16 01:33:43 +0530

    intial commit

---
+++ src/products.js
@@ -0,0 +1,65 @@
+import product1 from "./assets/11.jpg";
+
+import product2 from "./assets/17.png";
+import product3 from "./assets/14.jpg";
+import product4 from "./assets/16.jpg";
+import product5 from "./assets/redme.webp";
+import product6 from "./assets/xiomi.webp";
+
+// import product6 from "./assets/6.webp";
+// import product7 from "./assets/7.webp";
+// import product8 from "./assets/8.webp";
+
+
+
+
+export const PRODUCTS = [
+    {
+      id: 1,
+      productName: "Samsung Glaxay 521 Ulrta 5G",
+      price: 289.0,
+      productImage: product1,
+    },
+    {
+      id: 2,
+      productName: "Redmi Note 10 Pro",
+      price: 199.0,
+      productImage: product2,
+    },
+    {
+      id: 3,
+      productName: "Vivo top | 5G ",
+      price: 169.0,
+      productImage: product3,
+    },
+    {
+      id: 4,
+      productName: "OnePlus 8T 5G,silver",
+      price: 299.0,
+      productImage: product4,
+    },
+    {
+      id: 5,
+      productName: "Redmi Note 12 Pro+ 5G",
+      price: 19.99,
+      productImage: product5,
+    },
+      {
+        id: 6,
+       productName: "xiaomi 12 Pro | 5G ",
+       price: 68.0,
+       productImage: product6,
+     },
+    // {
+    //   id: 7,
+    //   productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    //   price: 120.0,
+    //   productImage: product7,
+    // },
+    // {
+    //   id: 8,
+    //   productName: "DON'T TRIP UNSTRUCTURED HAT",
+    //   price: 40.0,
+    //   productImage: product8,
+    // },
+  ];
